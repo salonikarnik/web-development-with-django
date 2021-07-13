6059abfc9aa537f00a37300b6769bf393acc1c75
Saloni Karnik <salonikarnik21@gmail.com>
2021-07-13 02:43:41 -0400

    tuples

---
+++ src/tuples.py
@@ -0,0 +1,20 @@
+#tuples are immutable
+
+three_numbers = (1, 2, 3)
+strings = ('home', 'land', 'earth')
+boo = (True, False, True)
+print(three_numbers)
+print(strings)
+print(boo)
+
+#get an element from the tuple
+print(three_numbers[0])
+
+#cannot assign a value within a tuple to an item once defined
+
+#length of a tuple
+print(len(three_numbers))
+
+#type
+print(type(three_numbers))
+
