afa80bf67ae89fd12bc6a63029916484b98e145a
Saloni Karnik <salonikarnik21@gmail.com>
2021-07-18 02:19:07 -0400

    dictionaries in python

---
+++ lessons/dictionaries.py
@@ -0,0 +1,26 @@
+#different data types can be used within a dictionary
+my_dict = {
+    'name': 'Tim',    
+    'nationality': 'American',
+    'age': 87,
+    'isTall': True,
+    'Qualification': 'College',
+    'friends': ['Peter', 'Paul', 'Precious']
+}
+print(my_dict)
+
+#print an element from the dictionary
+print(my_dict['name'])
+
+#lenth of dictionary
+print(len(my_dict))
+
+print(my_dict['age'])
+
+print(my_dict['friends'])
+
+#to check if it's a dictionary
+print(type(my_dict))
+
+x = my_dict['name']
+print(x)
