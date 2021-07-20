80c3e54335e2e5a10b4fe19068cba252cf414d81
Saloni Karnik <salonikarnik21@gmail.com>
2021-07-19 23:28:22 -0400

    comments in python

---
+++ lessons/comments.py
@@ -0,0 +1,10 @@
+#Add hashtag to comment out code
+#print(1)
+
+print('hello') #this line prints out hello
+
+#begin and end with three quotation marks to comment a block of code
+'''
+def my_func()
+'''
+
