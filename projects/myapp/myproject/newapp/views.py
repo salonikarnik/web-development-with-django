d5d62b2f5abac619fc75f82e23933cdbf791bd45
Saloni Karnik <salonikarnik21@gmail.com>
2021-08-05 23:24:11 -0400

    send data into template file

--- projects/myapp/myproject/newapp/views.py
+++ projects/myapp/myproject/newapp/views.py
@@ -3,5 +3,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'name': 'Patrick',
+        'age': 23,
+        'nationality': 'British'
+    }
+    return render(request, 'index.html', context)
  
